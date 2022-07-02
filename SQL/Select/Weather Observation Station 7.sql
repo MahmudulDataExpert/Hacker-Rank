ad103ac5640636d052340b653009437fbb14c652
Md. Mahmudul Hasan <82782893+mahmuduldata@users.noreply.github.com>
2022-07-02 17:54:32 +0600

    Create Weather Observation Station 7.sql

---
+++ SQL/Select/Weather Observation Station 7.sql
@@ -0,0 +1,21 @@
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+Input Format
+The STATION table is described as follows:
+------------------------------
+| Field       | Type         |
+------------------------------
+| ID          | NUMBER       |
+| CITY        | VARCHAR2(17) |
+| STATE       | VARCHAR2(3)  |
+| LAT_IN      | VARCHAR2(20) |
+| LONG_W      | NUMBER       |
+------------------------------
+
+Solution:
+SELECT DISTINCT(CITY)
+    FROM STATION
+WHERE CITY LIKE "%a"
+    or City like '%e' 
+    or City like '%i' 
+    or City like '%o' 
+    or City like '%u';
