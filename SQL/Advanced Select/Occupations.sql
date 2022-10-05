2b4e4bc88a91b9c7ed83a3b75980e80dd1931734
Md. Mahmudul Hasan <82782893+mahmuduldata@users.noreply.github.com>
2022-10-06 01:38:01 +0600

    Update Occupations.sql

--- SQL/Advanced Select/Occupations.sql
+++ SQL/Advanced Select/Occupations.sql
@@ -1,3 +1,27 @@
-Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
+/*
+pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
 
 Note: Print NULL when there are no more names corresponding to an occupation.
+*/
+
+SELECT
+    Doctor,
+    Professor,
+    Singer,
+    Actor
+FROM(
+    SELECT
+        NameOrder,
+        max(case Occupation when 'Doctor' then Name end) AS Doctor,
+        max(case Occupation when 'Professor' then Name end) AS Professor,
+        max(case Occupation when 'Singer' then Name end) AS Singer,
+        max(case Occupation when 'Actor' then Name end) AS Actor
+    FROM (
+            SELECT
+                Occupation,
+                Name,
+                row_number() over(partition BY Occupation ORDER BY Name ASC) AS NameOrder
+            FROM Occupations
+         ) AS NameLists
+    GROUP BY NameOrder
+    ) AS Names
