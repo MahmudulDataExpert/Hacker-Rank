b0a19d19e3ac2b42db1ffacb292bdc15a90c87d3
Md. Mahmudul Hasan <82782893+mahmuduldata@users.noreply.github.com>
2022-10-24 01:42:33 +0600

    Update Population Census.sql

--- SQL/Basic Join/Population Census.sql
+++ SQL/Basic Join/Population Census.sql
@@ -1,3 +1,7 @@
 /*
 Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
 */
+Solution:
+SELECT SUM(city.population)
+FROM CITY INNER JOIN COUNTRY ON CITY.countrycode=COUNTRY.code
+WHERE Continent='Asia';
