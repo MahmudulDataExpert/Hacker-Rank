31871afca63808bd010087e61e44b13e786cc8b4
Md. Mahmudul Hasan <82782893+mahmuduldata@users.noreply.github.com>
2023-07-26 00:19:11 +0600

    Population Census.sql

--- SQL/Basic Join/Population Census.sql
+++ SQL/Basic Join/Population Census.sql
@@ -3,5 +3,7 @@ Given the CITY and COUNTRY tables, query the sum of the populations of all citie
 */
 Solution:
 SELECT SUM(city.population)
-FROM CITY INNER JOIN COUNTRY ON CITY.countrycode=COUNTRY.code
+FROM CITY 
+  INNER JOIN COUNTRY 
+  ON CITY.countrycode=COUNTRY.code
 WHERE Continent='Asia';
